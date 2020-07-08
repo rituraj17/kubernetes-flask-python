7b9f39efe2cb0f313583a96b4287d65aab04b424
Rituraj <krishna_rituraj17@hotmail.com>
2020-07-08 06:19:10 +0530

    Updated code

---
+++ flask-api-v2/main.py
@@ -0,0 +1,12 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "This is V2 version "
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
